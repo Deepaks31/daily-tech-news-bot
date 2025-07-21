39ef169604dd7657903967c08b7d4970e6eec0e6
Deepak S <122810454+Deepaks31@users.noreply.github.com>
2025-07-21 19:22:45 +0530

    Update daily_tech_news_bot.py

--- daily_tech_news_bot.py
+++ daily_tech_news_bot.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import html
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
@@ -8,16 +9,25 @@ def get_tech_news():
     url = "https://hn.algolia.com/api/v1/search?tags=story&query=technology"
     res = requests.get(url).json()
     top_articles = res['hits'][:5]
-    news = "\n\n".join([f"🔹 {a['title']}\n{a['url']}" for a in top_articles])
-    return f"📰 *Today's Tech News*\n\n{news}"
+
+    news = ""
+    for a in top_articles:
+        title = html.escape(a['title'])
+        link = html.escape(a['url'])
+        news += f"🔹 <b>{title}</b><br><a href=\"{link}\">{link}</a><br><br>"
+
+    return f"📰 <b>Today's Tech News</b><br><br>{news}"
 
 def send_to_telegram(message):
     telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    data = {"chat_id": CHAT_ID, "text": message, "parse_mode": "Markdown"}
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message,
+        "parse_mode": "HTML"
+    }
     response = requests.post(telegram_url, data=data)
     print("Status:", response.status_code)
     print("Response:", response.text)
 
-if __name__ == "__main__":
-    msg = get_tech_news()
-    send_to_telegram(msg)
+msg = get_tech_news()
+send_to_telegram(msg)
